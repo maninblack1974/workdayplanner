10c5d55872e18d7ab7dcbca08ad37f7c1a42b735
DRH <25594179+maninblack1974@users.noreply.github.com>
2020-10-31 15:15:30 -0700

    adding psuedocode to my javascript file

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
+// Declare variables need for Workday Planner.
 
+// When planner is open the Current Date should display at the top of the page.
+
+// Time blocks display for standard business hours.
+
+// Blocks are color coded to tell the user if it is in the past gray), present(red), and future(green).
+
+// Clicking into a time block allows the user to enter an event.
+
+// After the user clicks the save button it will be saved in local storage.
+
+//When user refreshes the page it should display the saved events.
