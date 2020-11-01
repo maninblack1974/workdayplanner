c3af22c5b33773043a96f50cf76de14b92cdd90d
DRH <25594179+maninblack1974@users.noreply.github.com>
2020-10-31 23:50:17 -0700

    adding final code, still need some polish

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,60 @@
 // Declare variables need for Workday Planner.
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#13");
+var hour2 = $("#14");
+var hour3 = $("#15");
+var hour4 = $("#16");
+var hour5 = $("#17");
+var time = moment();
 
 // When planner is open the Current Date should display at the top of the page.
+//When user refreshes the page it should display the saved events.
 
-// Time blocks display for standard business hours.
+function setPlanner() {
 
-// Blocks are color coded to tell the user if it is in the past gray), present(red), and future(green).
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-// Clicking into a time block allows the user to enter an event.
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id);
+
+        if (schedule !== null) {
+            $(this).children(".schedule").val(schedule);
+        }
+    });
+}
 
 // After the user clicks the save button it will be saved in local storage.
+setPlanner();
+var saveBtn = $(".saveBtn");
 
-//When user refreshes the page it should display the saved events.
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+});
+
+// Blocks are color coded to tell the user if it is in the past gray), present(red), and future(green).
+// Time blocks display for standard business hours.
+function pastPresentFuture() {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+pastPresentFuture();
